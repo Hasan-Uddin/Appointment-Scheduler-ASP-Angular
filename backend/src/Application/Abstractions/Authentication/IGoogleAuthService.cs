79b72014061a18309092d68999e407f440cdb732
Hasan Uddin <abcnizamd@gmail.com>
2026-02-17 18:50:47 +0600

    Google Auth Interfaces

---
+++ backend/src/Application/Abstractions/Authentication/IGoogleAuthService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Application.Abstractions.Authentication;
+
+public interface IGoogleAuthService
+{
+    Task<GoogleUserInfo> ExchangeCodeAsync(string code);
+}
+
+public class GoogleUserInfo
+{
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string PictureUrl { get; set; }
+    public string GoogleId { get; set; }
+}
