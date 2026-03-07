edca66aca166833b08391058718d90efc8973f30
Hasan Uddin <abcnizamd@gmail.com>
2026-03-07 19:34:36 +0600

    Avaulable Slots

---
+++ backend/src/Application/Features/Available/AvailableSlots/GetAvailableTImeSlot/GetTimeSlotsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Features.Available.AvailableSlots.GetAvailableTImeSlot;
+
+public sealed record GetTimeSlotsQuery(
+    Guid EventTypeId,
+    DateTime Date
+) : IQuery<List<TimeSlotResponse>>;
---
+++ backend/src/Application/Features/Available/AvailableSlots/GetMontlyList/AvailableDayResponse.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.Features.Available.AvailableSlots.GetMontlyList;
+
+public sealed class AvailableDayResponse
+{
+    public string Date { get; init; } = default!; // yyyy-MM-dd
+    public bool HasSlots { get; init; }
+    public int SlotsCount { get; init; }
+}
